134464b76b78746504083b41ba9783f1c6770468
Michael Quach <mquac089@uottawa.ca>
2024-06-13 11:47:27 -0400

    uploaded mock

--- lab05/grades/lib/grades.ex
+++ lab05/grades/lib/grades.ex
@@ -6,4 +6,20 @@ defmodule Grades do
   Contexts are also responsible for managing your data, regardless
   if it comes from the database, an external API or others.
   """
+  
+  #alias Grades.calculator
+	defmodule Calculator do
+		def letter_grade(_input) do
+			"A+"
+		end
+	
+		def percentage_grade(_grades) do
+			95.0 
+		end
+
+		def numeric_grade(_grades) do
+			4.0 
+		end
+	end 
+  
 end
